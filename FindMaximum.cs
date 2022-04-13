a544415f31808a812e0adfdd925cfb659b19d2fa
saikiranreddy <saikiranreddy@gmail.com>
2022-04-14 00:00:31 +0530

    [kiran]-Added FindMaxOf3Integers

---
+++ FindMaximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    internal class FindMaximum
+    {
+        public int GetMaxOfThree(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            else if (num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaximum findmaximum = new FindMaximum();
+            int intmax = findmaximum.GetMaxOfThree(96,45,76);
+            Console.WriteLine("Maximum of three integers is : " + intmax);
+            Console.ReadLine();
+        }
+    }
+}
