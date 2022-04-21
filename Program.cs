680934cc19fe5c29a85432148693601d784cd1f3
saikiranreddy96 <98898706+saikiranreddy96@users.noreply.github.com>
2022-04-22 01:59:46 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,19 +10,19 @@ namespace FindMaxUsingGenerics
     {
         static void Main(string[] args)
         {
-            //Maximum integer
+            //Maximum integer(UC1)
             FindMaximum findMaximum1 = new FindMaximum();
             int intmax = findMaximum1.MaximumInteger(45,58,26);
             Console.WriteLine("Maximum of three integers is : " + intmax);
 
-            //Maximum float
+            //Maximum float(UC2)
             FindMaximum findMaximum2 = new FindMaximum();
             float floatmax = findMaximum2.MaximumFloat(12.3f, 45.3f, 36.1f);
             Console.WriteLine("Maximum of three floats is : " + floatmax);
 
-            //Maximum string
+            //Maximum string(UC3)
             FindMaximum findMaximum3 = new FindMaximum();
-            string stringmax = findMaximum3.MaximumString("apple","ball","cat");
+            string stringmax = findMaximum3.MaximumString("Orange","Apple","Mango");
             Console.WriteLine("Maximum of three strings is : " + stringmax);
             Console.ReadLine();
         }
